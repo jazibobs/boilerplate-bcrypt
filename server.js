3493273c0e4206cb84540b30bd3c9041c51dea9c
Jared Rigby <talk@jaredrigby.co.uk>
2022-03-15 23:25:07 +0800

    Testing bcrypt

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-bcrypt.hash("passw0rd!", 13, (err, hash) => {
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
   console.log(hash);
   //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
-  bcrypt.compare("passw0rd!", hash, (err, res) => {
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
     console.log(res); //true
   });
 });
